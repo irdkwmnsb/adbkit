9570e9f6a1998cf6e86ea3a9c1cecd91637f37b3
UrielCh <uriel.chemouni@gmail.com>
2022-03-03 12:10:53 +0200

    clean test

--- test/adb/command/host-transport/ipRoute.ts
+++ test/adb/command/host-transport/ipRoute.ts
@@ -6,31 +6,19 @@ import { testParser, testTansport } from './commonTest';
 
 
 describe('ipRouteCommand', function () {
-    it("should send 'ip route'", function () {
-        return testTansport(IpRouteCommand, 'shell:ip route');
-    });
+    it("should send 'ip route'", () => testTansport(IpRouteCommand, 'shell:ip route'));
 
-    it("should send 'ip route list table all'", function () {
-        return testTansport(IpRouteCommand, 'shell:ip route list table all', ['list table all']);
-    });
+    it("should send 'ip route list table all'", () => testTansport(IpRouteCommand, 'shell:ip route list table all', ['list table all']));
 
-    it("should send 'ip route list table all' split", function () {
-        return testTansport(IpRouteCommand,
-            'shell:ip route list table all',
-            ['list', 'table', 'all']);
-    });
+    it("should send 'ip route list table all' split", () => testTansport(IpRouteCommand, 'shell:ip route list table all', ['list', 'table', 'all']));
 
-    it('should return a list of routes', (): Promise<void> => {
-        return testParser(IpRouteCommand,
-            `192.168.1.0/24 dev wlan0 proto kernel scope link src 192.168.1.2\n`,
-            [{ dest: '192.168.1.0/24', dev: 'wlan0', proto: 'kernel', scope: 'link', src: '192.168.1.2' }],
-        )
-    });
+    it('should return a list of routes', () => testParser(IpRouteCommand,
+        `192.168.1.0/24 dev wlan0 proto kernel scope link src 192.168.1.2\n`,
+        [{ dest: '192.168.1.0/24', dev: 'wlan0', proto: 'kernel', scope: 'link', src: '192.168.1.2' }],
+    ));
 
-    it('should return convert number string to numbers', async (): Promise<void> => {
-        return testParser(IpRouteCommand,
-            `fe80::/64 dev wlan0 table 1021 proto kernel metric 256 pref medium\n`,
-            [{ dest: 'fe80::/64', dev: 'wlan0', table: 1021, proto: 'kernel', metric: 256, pref: 'medium' }]
-        )
-    });
+    it('should return convert number string to numbers', () => testParser(IpRouteCommand,
+        `fe80::/64 dev wlan0 table 1021 proto kernel metric 256 pref medium\n`,
+        [{ dest: 'fe80::/64', dev: 'wlan0', table: 1021, proto: 'kernel', metric: 256, pref: 'medium' }]
+    ));
 });
--- test/adb/command/host-transport/ps.ts
+++ test/adb/command/host-transport/ps.ts
@@ -1,53 +1,28 @@
-import Chai, { expect } from 'chai';
+import Chai from 'chai';
 import simonChai from 'sinon-chai';
 Chai.use(simonChai);
-import MockConnection from '../../../mock/connection';
-import Protocol from '../../../../src/adb/protocol';
-import PsCommand, { PsEntry } from '../../../../src/adb/command/host-transport/ps';
+import PsCommand from '../../../../src/adb/command/host-transport/ps';
 
 import { testParser, testTansport } from './commonTest';
 
 describe('psCommand', function () {
-    it("should send 'ps'", function () {
-        return testTansport(PsCommand, 'shell:ps');
-    });
+    it("should send 'ps'", () => testTansport(PsCommand, 'shell:ps'));
 
-    it("should send 'ps -f'", function () {
-        return testTansport(PsCommand, 'shell:ps -f', [ '-f' ]);
-    });
+    it("should send 'ps -f'", () => testTansport(PsCommand, 'shell:ps -f', ['-f']));
 
-    it('should return a list of PsEntry', async (): Promise<void> => {
-        const conn = new MockConnection();
-        const cmd = new PsCommand(conn);
-        setImmediate(function () {
-            conn.getSocket().causeRead(Protocol.OKAY);
-            conn.getSocket().causeRead(`UID             PID   PPID C STIME TTY          TIME CMD
+    it('should return a list of PsEntry', () => testParser(PsCommand,
+        `UID             PID   PPID C STIME TTY          TIME CMD
 shell         30941   3021 0 15:35:50 pts/3 00:00:00 sh
 shell         31350  30941 21 16:18:23 pts/3 00:00:00 ps -f
-`);
-            return conn.getSocket().causeEnd();
-        });
-        const processList: Partial<PsEntry>[] = await cmd.execute();
-        expect(processList).to.eql([
-            { UID: 'shell', PID: '30941', PPID: '3021', C: '0', STIME: '15:35:50', TTY: 'pts/3', TIME: '00:00:00', CMD: 'sh' },
-            { UID: 'shell', PID: '31350', PPID: '30941', C: '21', STIME: '16:18:23', TTY: 'pts/3', TIME: '00:00:00', CMD: 'ps -f' },
-        ]);
-    });
+`, [
+        { UID: 'shell', PID: '30941', PPID: '3021', C: '0', STIME: '15:35:50', TTY: 'pts/3', TIME: '00:00:00', CMD: 'sh' },
+        { UID: 'shell', PID: '31350', PPID: '30941', C: '21', STIME: '16:18:23', TTY: 'pts/3', TIME: '00:00:00', CMD: 'ps -f' },
+    ]))
 
-    it('should return a list of PsEntry -A', async (): Promise<void> => {
-        const conn = new MockConnection();
-        const cmd = new PsCommand(conn);
-        setImmediate(function () {
-            conn.getSocket().causeRead(Protocol.OKAY);
-            conn.getSocket().causeRead(`USER            PID   PPID     VSZ    RSS WCHAN            ADDR S NAME
+    it('should return a list of PsEntry -A', () => testParser(PsCommand, `USER            PID   PPID     VSZ    RSS WCHAN            ADDR S NAME
 root              1      0 10826728  3728 0                   0 S init
-root              2      0       0      0 0                   0 S [kthreadd]`);
-            return conn.getSocket().causeEnd();
-        });
-        const processList: Partial<PsEntry>[] = await cmd.execute();
-        expect(processList).to.eql([
-            { USER: 'root', PID: '1', PPID: '0', VSZ: '10826728', RSS: '3728', WCHAN: '0', ADDR: '0', S: 'S', NAME: 'init' },
-            { USER: 'root', PID: '2', PPID: '0', VSZ: '0', RSS: '0', WCHAN: '0', ADDR: '0', S: 'S', NAME: '[kthreadd]' },
-        ]);
-    });
+root              2      0       0      0 0                   0 S [kthreadd]`, [
+        { USER: 'root', PID: '1', PPID: '0', VSZ: '10826728', RSS: '3728', WCHAN: '0', ADDR: '0', S: 'S', NAME: 'init' },
+        { USER: 'root', PID: '2', PPID: '0', VSZ: '0', RSS: '0', WCHAN: '0', ADDR: '0', S: 'S', NAME: '[kthreadd]' },
+    ]));
 });
