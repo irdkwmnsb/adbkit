b8e1888a22ab4bead35ab0e27d7b227e67c5dcb9
UrielCh <uriel.chemouni@gmail.com>
2022-03-05 04:34:50 +0200

    commit sample test code

--- tasks/realTest.ts
+++ tasks/realTest.ts
@@ -1,4 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import adb from '../src/adb';
+import { IpRouteEntry, IpRuleEntry } from '../src/adb/command/host-transport';
+
+function print(list: Array<IpRouteEntry | IpRuleEntry>) {
+  for (const route of list) console.log(route.toString());
+}
+
 
 const main = async () => {
   const adbClient = adb.createClient();
@@ -7,14 +14,24 @@ const main = async () => {
     console.error('Need at least one connected android device');
     return;
   }
-  const client = devices[0].getClient();
-  const rules = await client.getIpRule('list');
-  for (const rule of rules)
-    console.log(rule.toStirng());
+  const deviceClient = devices[0].getClient();
+  deviceClient.sudo = true;
+  const rules = await deviceClient.ipRule('list');
+  // print(rules)
+  const routes = await deviceClient.ipRoute('list', 'table', 'all');
+  // print(routes)
 
-  const routes = await client.getIpRoute('list', 'table', 'all');
-  for (const route of routes)
-    console.log(route.toString());
+  const routesWifi = await deviceClient.ipRoute('show table wlan0');
+  // print(routesWifi)
+  
+  //console.log('4g');
+  const routes4G = await deviceClient.ipRoute('show table rmnet_data2');
+  // print(routes4G)
+   
+  const defaultWifi = routesWifi.find(r => r.dest === 'default')
+  const default4G = routes4G.find(r => r.dest === 'default')
+  console.log(`default Wifi is: ${defaultWifi}`);
+  console.log(`default   4G is: ${default4G}`);
 }
 
 main();
