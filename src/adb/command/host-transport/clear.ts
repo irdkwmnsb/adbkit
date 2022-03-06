047f4fa183770a7852dcb35e5392b089add17bd6
UrielCh <uriel.chemouni@gmail.com>
2022-03-06 22:00:47 +0200

    remove ~370 line of boilerplate

--- src/adb/command/host-transport/clear.ts
+++ src/adb/command/host-transport/clear.ts
@@ -3,27 +3,20 @@ import Command from '../../command';
 export default class ClearCommand extends Command<boolean> {
   async execute(pkg: string): Promise<boolean> {
     this._send(`shell:pm clear ${pkg}`);
-    const reply = await this.parser.readAscii(4);
-    switch (reply) {
-      case this.protocol.OKAY:
-        try {
-          const result = await this.parser.searchLine(/^(Success|Failed)$/)
-          switch (result[0]) {
-            case 'Success':
-              return true;
-            case 'Failed':
-              // Unfortunately, the command may stall at this point and we
-              // have to kill the connection.
-              throw new Error(`Package '${pkg}' could not be cleared`);
-          }
-          return false;
-        } finally {
-          this.parser.end()
-        }
-      case this.protocol.FAIL:
-        return this.parser.readError();
-      default:
-        return this.parser.unexpected(reply, 'OKAY or FAIL');
+    await this.readOKAY();
+    try {
+      const result = await this.parser.searchLine(/^(Success|Failed)$/)
+      switch (result[0]) {
+        case 'Success':
+          return true;
+        case 'Failed':
+          // Unfortunately, the command may stall at this point and we
+          // have to kill the connection.
+          throw new Error(`Package '${pkg}' could not be cleared`);
+      }
+      return false;
+    } finally {
+      this.parser.end()
     }
   }
 }
