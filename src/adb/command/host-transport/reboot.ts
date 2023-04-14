fe4d69df50bb759286f895f99cc166066435d45e
NoahAndrews <noah@revrobotics.com>
2023-04-14 17:44:37 -0500

    Add 'sideload-auto-reboot' to RebootType

--- src/adb/command/host-transport/reboot.ts
+++ src/adb/command/host-transport/reboot.ts
@@ -1,6 +1,6 @@
 import Command from '../../command';
 
-export type RebootType = 'bootloader' | 'recovery' | 'sideload' | 'fastboot';
+export type RebootType = 'bootloader' | 'recovery' | 'sideload' | 'fastboot' | 'sideload-auto-reboot';
 
 export default class RebootCommand extends Command<true> {
   async execute(type?: RebootType): Promise<true> {
